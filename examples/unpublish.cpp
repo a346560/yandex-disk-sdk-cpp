78e425432335bb917f36e0274dff016e4aca93d2
a346560 <a346560@ya.ru>
2017-03-17 12:29:30 +0400

    Update unpublish.cpp

--- examples/unpublish.cpp
+++ examples/unpublish.cpp
@@ -10,17 +10,17 @@ int main () {
   
   url::path resource = "existed_file";
   auto info = client.unpublish(resource);
-  auto href = info["href"].as_string();
+  auto code = info["code"].as_string();
   cout << href << endl;
   
   url::path resource = "not_existed_file";
   auto info = client.unpublish(resource);
-  auto code = info["error"].as_string();
+  auto code = info["code"].as_string();
   cout << code << endl;
   
   url::path resource = "existed_dir_no_full";
   auto info = client.unpublish(resource);
-  auto code = info["error"].as_string();
+  auto code = info["code"].as_string();
   auto href = info["href"].as_string();
   cout << code << href <<endl;
   
